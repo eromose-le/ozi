d054f88016c0846c4a773994d7b7ccdb6d6736cd
eromose-le <erons24@yahoo.com>
2021-04-22 00:23:13 +0100

    ..

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/report', [ReportController::class, 'index'])
 // NORMAL ROUTE ____________________________________________________________
 Route::get('/', function () {
     return view('welcome');
-});
+}); 
 
 
 // APP ROUTES ______________________________________________________________
