2291cec377ed380775d8bfcc9dd2b7fc2f618e67
eromose-le <erons24@yahoo.com>
2021-04-21 23:44:47 +0100

    changed branch name to superrole and homepage route

--- routes/web.php
+++ routes/web.php
@@ -53,9 +53,9 @@ Route::get('/report', [ReportController::class, 'index'])
 
 
 // NORMAL ROUTE ____________________________________________________________
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 // APP ROUTES ______________________________________________________________
@@ -77,7 +77,7 @@ Route::get('/status', function () {
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
